f81fcfd89b8c65c794fa90dfd3ea0c643deb7998
Julio Aenlle <gmg8r251@ufl.edu>
2016-06-04 01:22:00 -0400

    Modify index file with row & column divs for grid
    Addition of three div elements - header, section & footer - to html
    file. Followed by addition of content. Then the addition of the
    container <div> to hold them all plus the addition of <div>s with
    class "row" followed by <div>s with class "column".

--- index.php
+++ index.php
@@ -7,10 +7,31 @@
     <link rel="stylesheet" type="text/css" href ="
     <?php bloginfo('stylesheet_url'); ?>" />
   </head>
-  <body>
 
-    <h1>Julio's Blank WordPress Template</h1>
-    <p>This is my WordPress template.</p>
+  <body>
+    <div class="container">
+
+      <header class="row">
+        <div class="twelve columns">
+            <h1>Julio's Blank WordPress Template</h1>
+            <p>This is my WordPress template.</p>
+        </div>
+      </header>
+
+      <section class="row">
+        <div class="twelve columns">
+          <h2>Section Content - cool</h2>
+        </div>
+      </section>
+
+      <footer class="row">
+        <div class="twelve columns">
+          <h2>Footer Stuff</h2>
+        </div>
+      </footer>
+
+    </div>
+
 
   </body>
 </html>
